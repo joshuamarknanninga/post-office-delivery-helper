d0b4ac553a05ac78e81ee38ec58df12158736b0e
Joshua Nanninga <jmarknanninga@gmail.com>
2024-11-12 20:02:27 -0600

    added ProtectedRoutes

---
+++ frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const { token } = useContext(AuthContext);
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                token ? <Component {...props} /> : <Redirect to="/login" />
+            }
+        />
+    );
+};
+
+export default ProtectedRoute;
